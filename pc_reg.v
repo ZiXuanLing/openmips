9a41d6a3fd919bf8bdeaf4477dcfaef7389d7858
ZiXuanLing <2226958871@qq.com>
2022-05-18 01:09:57 +0800

    lh

--- pc_reg.v
+++ pc_reg.v
@@ -11,7 +11,10 @@ module pc_reg (
     input wire[5:0] stall,  // 来自控制模块CTRL
 
     output reg[`InstAddrBus] pc,  // pc
-    output reg ce  // 指令存储器使能信号
+    output reg ce,  // 指令存储器使能信号
+
+    input wire branch_flag_i,  // 是否发生转移
+    input wire[`RegBus] branch_target_address_i  // 转移到的目标地址
 );
     always @(posedge clk) begin
         if (rst == `RstEnable) begin
@@ -26,7 +29,11 @@ module pc_reg (
             pc <= 32'h00000000;
         // 当stall[0]为NoStop时，pc加4，否则，pc保持不变
         end else if (stall[0] == `NoStop) begin
-            pc <= pc + 4'h4;
+            if (branch_flag_i == `Branch) begin
+                pc <= branch_target_address_i;
+            end else begin
+                pc <= pc + 4'h4;
+            end
         end
     end
 endmodule
